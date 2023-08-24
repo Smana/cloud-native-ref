c2d798a9aa2d5fa500f5646c39da08995a17e862
Smaine Kahlouch <smainklh@gmail.com>
2023-08-24 21:46:08 +0200

    chore: upgrade Cilium 1.14.1

--- terraform/eks/variables.tf
+++ terraform/eks/variables.tf
@@ -30,7 +30,7 @@ variable "cluster_version" {
 
 variable "cilium_version" {
   description = "Cilium cluster version"
-  default     = "1.14.0"
+  default     = "1.14.1"
   type        = string
 }
 
