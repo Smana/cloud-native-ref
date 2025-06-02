aaf3d8ef237f58b37f0a6a7b136228941d38d8da
SmaineTF1 <smainklh@gmail.com>
2025-06-02 21:39:08 +0200

    chore: upgrade flux operator 0.31.0

--- opentofu/eks/variables.tf
+++ opentofu/eks/variables.tf
@@ -91,7 +91,7 @@ variable "cert_manager_approle_secret_name" {
 
 variable "flux_operator_version" {
   description = "Flux Operator version"
-  default     = "0.20.0"
+  default     = "0.22.0"
   type        = string
 }
 
--- opentofu/openbao/cluster/variables.tf
+++ opentofu/openbao/cluster/variables.tf
@@ -22,7 +22,7 @@ variable "mode" {
 variable "openbao_version" {
   description = "OpenBao version to install"
   type        = string
-  default     = "2.2.1"
+  default     = "2.2.2"
 }
 
 variable "openbao_data_path" {
