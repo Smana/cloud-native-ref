9bad4739e58db49112d967d316fde8c7b34c9789
SmaineTF1 <smainklh@gmail.com>
2025-06-28 09:46:16 +0200

    fix(cert-manager): update CRDs that match the deployed version

--- opentofu/openbao/management/auth.tf
+++ opentofu/openbao/management/auth.tf
@@ -1,19 +1,25 @@
-resource "vault_auth_backend" "approle" {
-  namespace = vault_namespace.pki.namespace
+resource "vault_auth_backend" "approle_admin" {
+  namespace = vault_namespace.admin.path_fq
   type      = "approle"
 }
 
 resource "vault_approle_auth_backend_role" "snapshot" {
-  namespace         = vault_auth_backend.approle.namespace
-  backend           = vault_auth_backend.approle.path
+  namespace         = vault_namespace.admin.path
+  backend           = vault_auth_backend.approle_admin.path
   role_name         = "snapshot-agent"
   token_policies    = ["snapshot"]
   token_bound_cidrs = var.allowed_cidr_blocks
 }
 
+
+resource "vault_auth_backend" "approle_pki" {
+  namespace = vault_namespace.pki.path_fq
+  type      = "approle"
+}
+
 resource "vault_approle_auth_backend_role" "cert_manager" {
-  namespace         = vault_auth_backend.approle.namespace
-  backend           = vault_auth_backend.approle.path
+  namespace         = vault_namespace.pki.path_fq
+  backend           = vault_auth_backend.approle_pki.path
   role_name         = "cert-manager"
   token_policies    = ["cert-manager"]
   token_bound_cidrs = var.allowed_cidr_blocks
