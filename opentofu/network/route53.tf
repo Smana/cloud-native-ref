3337eb10ea5ef42fcf88e5dcdbc8344fd690a04c
Smana <smainklh@gmail.com>
2025-10-04 09:39:33 +0200

    🔧 chore: migrate Route53 module to v6 structure
    - Update module source from modules/zones to base module
    - Refactor configuration from zones map to direct zone config
    - Change vpc parameter from list to map structure
    - Addresses Route53 v6 breaking changes where modules/zones was removed
    
    Required state migration:
    tofu state mv 'module.zones.aws_route53_zone.this["priv.cloud.ogenki.io"]' 'module.zones.aws_route53_zone.this[0]'

--- opentofu/network/route53.tf
+++ opentofu/network/route53.tf
@@ -1,18 +1,17 @@
 module "zones" {
-  source  = "terraform-aws-modules/route53/aws//modules/zones"
+  source  = "terraform-aws-modules/route53/aws"
   version = "~> 6.0"
 
-  zones = {
-    "priv.cloud.ogenki.io" = {
-      comment = "Internal zone for private DNS hosts"
-      vpc = [
-        {
-          vpc_id = module.vpc.vpc_id
-        }
-      ]
-      tags = {
-        env = var.env
-      }
+  name    = "priv.cloud.ogenki.io"
+  comment = "Internal zone for private DNS hosts"
+
+  vpc = {
+    one = {
+      vpc_id = module.vpc.vpc_id
     }
+  }
+
+  tags = {
+    env = var.env
   }
 }
