2b154629f2c4fd58ee3a16f91da4d4aac2df6218
Smana <smainklh@gmail.com>
2026-02-12 15:03:18 +0100

    🐛 fix: update cosign identity for flux-operator chart and headlamp version
    - flux-operator OCIRepository: chart signing moved from
      controlplaneio-fluxcd/flux-operator to controlplaneio-fluxcd/charts
    - headlamp: version 0.41.0 doesn't exist, downgrade to 0.40.0

--- opentofu/openbao/cluster/versions.tf
+++ opentofu/openbao/cluster/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = "~> 6.0, < 6.32.0"
     }
     cloudinit = {
       source  = "hashicorp/cloudinit"
